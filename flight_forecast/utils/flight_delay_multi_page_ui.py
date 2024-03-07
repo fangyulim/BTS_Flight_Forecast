a8e465eb3a94f596b270e91c9b6d7cffe074fa10
Fiona <fangyu.lim@wsu.edu>
2024-03-06 19:03:54 -0800

    Uploading current progress for ui, and added a dummy airline list to test gui.

--- flight_forecast/utils/flight_delay_multi_page_ui.py
+++ flight_forecast/utils/flight_delay_multi_page_ui.py
@@ -7,6 +7,9 @@ a prediction of delay severity, or just delay prediction.
 The second page allows admin to upload more data and retrain the model.
 Comments: I ended up not putting state and airport name in option because
 not all airports have a name.
+
+To use this module, please use command: python -m utils.flight_delay_multi_page_ui in terminal.
+To run using IDE, please change file paths.
 """
 import csv
 import sys
@@ -37,12 +40,14 @@ class Milestone2V2(QMainWindow):
         self.user_int.setupUi(self)
         self.setup_ui()
 
+        # Moves to admin login page when " Admin Login" button is clicked
         self.user_int.admin_login_btn.clicked.connect(
             lambda: self.stacked_widget_pages.setCurrentIndex(1))
+        # Moves to main page when "Main Page" button is clicked
         self.user_int.main_page_btn.clicked.connect(
             lambda: self.stacked_widget_pages.setCurrentIndex(0))
 
-        # Setting labels to be centered
+        # Centering labels
         label_main_page = self.user_int.main_page_lb
         label_main_page.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
         label_login_page = self.user_int.login_page_lb
@@ -52,14 +57,17 @@ class Milestone2V2(QMainWindow):
 
         # Instantiating widgets and functions used on main page.
         self.load_airport_list()
-        # self.user_int.AirportSelection.currentTextChanged.connect(self.airport_changed)
+        self.load_airlines_list()
+
+        # This will be used if we decide to move airport_selection as it's own function.
+        # self.user_int.airport_selection.currentTextChanged.connect(self.airport_changed)
         self.user_int.PredictButton.clicked.connect(self.prediction)
 
-        # Authentication
+        # Authentication page.
         self.user_int.error_msg_lb.setVisible(False)
         self.user_int.login_btn.clicked.connect(self.authenticate)
 
-        # Upload
+        # Upload page.
         self.user_int.file_lb.setVisible(False)
         self.user_int.upload_btn.clicked.connect(self.upload_files)
         self.user_int.new_mod_lb.setVisible(False)
@@ -71,6 +79,9 @@ class Milestone2V2(QMainWindow):
     def setup_ui(self):
         """
         This function sets up the UI.
+        - Sets up the dimension of the UI.
+        - Instantiated stacked widget (pages).
+        - Sets label/ widgets to not appear before user input.
         """
         width = 1200
         height = 750
@@ -83,6 +94,7 @@ class Milestone2V2(QMainWindow):
         self.user_int.label_6.setVisible(False)
         self.user_int.prob_delay_result.setVisible(False)
         self.user_int.label_5.setVisible(False)
+        self.user_int.fail_predict_lb.setVisible(False)
 
     def load_airport_list(self):
         """
@@ -94,31 +106,30 @@ class Milestone2V2(QMainWindow):
             next(file)  # skips the header.
             airport_codes = [row[0] for row in csv.reader(file)]
 
-        # Clear the existing items in AirportSelection widget
-        self.user_int.AirportSelection.clear()
+        # Clear the existing items in airport_selection widget
+        self.user_int.airport_selection.clear()
 
         # Adding airport names to the widget.
-        self.user_int.AirportSelection.addItems(airport_codes)
+        self.user_int.airport_selection.addItems(airport_codes)
 
-    # def airport_changed(self):
-    #     """
-    #     This function is what happens when airport selection is changed.
-    #     Here it should obtain data for the relative airport.
-    #     Returns: airport code
-    #     """
-    #     # I'm just printing this out for now.
-    #     # Thinking afterwards this might not be necessary.
-    #     airport_selected =self.user_int.AirportSelection.currentText()
-    #     print(airport_selected)
-    #     return airport_selected
+    def load_airlines_list(self):
+        with open("../resources/airline_list.csv", "r", encoding="utf-8") as file:
+            print(file)
+            next(file)  # skips the header.
+            airline_list = [row[0] for row in csv.reader(file)]
+
+        self.user_int.airline_selection.clear()
+        self.user_int.airline_selection.addItems(airline_list)
+
+
 
     def prediction(self):
         """
         This function processes input information and displays prediction
         """
-        airport_selection = self.user_int.AirportSelection.currentText()
-
-        date_selection = self.user_int.DateSelection.date().toString("yy.MM.dd")
+        airport_selected = self.user_int.airport_selection.currentText()
+        airline_selected = self.user_int.airline_selection.currentText()
+        date_selection = self.user_int.date_selection.date().toString("yy.MM.dd")
         time_selection = self.user_int.TimeSelection.time().toString("HH:mm:ss")
 
         datetime_info = [
@@ -129,38 +140,54 @@ class Milestone2V2(QMainWindow):
             time_selection.split(":")[1],  # minute
             time_selection.split(":")[2]   # second
         ]
+        time_obj = datetime.strptime(time_selection, "%H:%M:%S")
 
-        # Weather needs time in seconds as integer.
-        # Include airline.
         # temp print to test pylint
         print(datetime_info)
+        print(time_obj)
+
+        total_seconds =time_obj.second + time_obj.minute * 60 + time_obj.hour * 3600
+
+        # Now 'total_seconds' contains the time represented by 'time_selection' in seconds
+        print(total_seconds)
+
+        # Weather needs time in seconds as integer.
+            # Needs to be in unix time so DATE -TIME -> Seconds
+
         # If selected day is within 15 days, then we are able to give a prediction
 
         current_date = datetime.now()
-        qdate = self.user_int.DateSelection.date()
+        qdate = self.user_int.date_selection.date()
         date_selected = datetime(qdate.year(), qdate.month(), qdate.day())
         difference = current_date - date_selected
         if difference.days < 15:
-            delay_prediction = airport_selection + date_selection + time_selection \
-                               + " including the forecast weather"
+            delay_prediction = airport_selected + date_selection + time_selection \
+                               + " forecast is: "
+            forecast = weather.get_weather_forecast(airport_selected, total_seconds)
+            #print(forecast)
+            if self.user_int.check_box.isChecked():
+
+                severity_prediction = airport_selected + date_selection + time_selection
+                self.user_int.avg_delay_result.setText(severity_prediction)
+                self.user_int.avg_delay_result.setVisible(True)
+                self.user_int.label_6.setVisible(True)
+
+            else:
+                self.user_int.avg_delay_result.setVisible(False)
+                self.user_int.label_6.setVisible(False)
+
+            self.user_int.prob_delay_result.setVisible(True)
+            self.user_int.label_5.setVisible(True)
+            self.user_int.fail_predict_lb.setVisible(False)
+            self.user_int.prob_delay_result.setText("The predicted delay is")
         else:
-            delay_prediction = airport_selection + date_selection + time_selection \
-                               + " without the forecast weather"
-
-        self.user_int.prob_delay_result.setVisible(True)
-        self.user_int.label_5.setVisible(True)
-        self.user_int.prob_delay_result.setText(delay_prediction)
-
-        if self.user_int.EstimateCheckBox.isChecked():
-
-            severity_prediction = airport_selection + date_selection + time_selection
-            self.user_int.avg_delay_result.setText(severity_prediction)
-            self.user_int.avg_delay_result.setVisible(True)
-            self.user_int.label_6.setVisible(True)
-
-        else:
+            self.user_int.fail_predict_lb.setVisible(True)
+            self.user_int.prob_delay_result.setVisible(False)
             self.user_int.avg_delay_result.setVisible(False)
+            self.user_int.label_5.setVisible(False)
             self.user_int.label_6.setVisible(False)
+            self.user_int.fail_predict_lb.setText("Sorry unable to predict!\n "
+                                                  "Please enter date within 15 days from today.")
 
     # Admin Page
     def authenticate(self):
